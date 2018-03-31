0d354bd1dbf21f122bd8f5d3d4107902cee6242c
SaeedAhmedX <saeedahmedn1@gmail.com>
2018-04-01 00:16:09 +0530

    Changes

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 int factorial(int number) {
     if(number == 0) 
-        return 1;\
+        return 1;
     return number * factorial(number - 1);
 }
 
@@ -11,7 +11,5 @@ int main() {
     printf("Factorial of 1 = %d\n", factorial(1));
     printf("Factorial of 2 = %d\n", factorial(2));
     printf("Factorial of 3 = %d\n", factorial(3));
-    printf("Factorial of 4 = %d\n", factorial(4));
-    printf("Factorial of 5 = %d\n", factorial(5));
     return 0;
 }
