da582ffb4ed2187052f1383dcba609b943ee9759
SaeedAhmedX <saeedahmedn1@gmail.com>
2018-04-01 00:08:05 +0530

    Factorial feature is added

--- main.c
+++ main.c
@@ -1,5 +1,17 @@
 #include<stdio.h>
+
+int factorial(int number) {
+    if(number == 0) 
+        return 1;\
+    return number * factorial(number - 1);
+}
+
 int main() {
-    printf("Hello world");
+    printf("Factorial of 0 = %d\n", factorial(0));
+    printf("Factorial of 1 = %d\n", factorial(1));
+    printf("Factorial of 2 = %d\n", factorial(2));
+    printf("Factorial of 3 = %d\n", factorial(3));
+    printf("Factorial of 4 = %d\n", factorial(4));
+    printf("Factorial of 5 = %d\n", factorial(5));
     return 0;
 }
